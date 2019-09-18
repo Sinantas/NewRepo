fe8feb3b89bfa31cefa3a15d90da625401af3cfc
sinan <sinantasfl@gmail.com66666666>
2019-09-17 21:16:42 -0400

    second commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,6 +1,9 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("2");
+        int a= 5;
+        int b= 6;
+        int number= a+b;
+        System.out.println(number);
     }
 }
