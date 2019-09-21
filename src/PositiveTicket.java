fe4d4b71d1b328ad1c833508388479d9033ebb64
sinan <sinantasfl@gmail.com66666666>
2019-09-21 17:15:47 -0400

    PositiveTicket_updated

--- src/PositiveTicket.java
+++ src/PositiveTicket.java
@@ -10,5 +10,9 @@ public class PositiveTicket {
         driver.findElement(By.xpath("//input[@type='email']")).sendKeys("Asude");
         Thread.sleep(1000);
         driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("pwd");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[contains(@type,'mai')]")).sendKeys("contains mai");
+
+
     }
 }
