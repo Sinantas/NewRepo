b88646f074bb552bac135ddafaae3195308f42be
sinan <sinantasfl@gmail.com66666666>
2019-09-21 16:14:41 -0400

    PositiveTicket

---
+++ src/PositiveTicket.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class PositiveTicket {
+    public static void main(String[] args) throws InterruptedException {
+        Browser myBrowser= new Browser();
+        WebDriver driver=myBrowser.invokeChromeBrowser();
+        driver.get("https://www.facebook.com");
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//input[@type='email']")).sendKeys("Asude");
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("pwd");
+    }
+}
