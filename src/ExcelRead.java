03147730e581777ecbc29a280565940b6896b93f
sinan <sinantasfl@gmail.com66666666>
2019-10-05 15:06:33 -0400

    ScreenShot

---
+++ src/ExcelRead.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.FileInputStream;
+
+public class ExcelRead {
+    public static void main(String[] args) {
+        File file = new File("C:\\Try.xls");
+        System.out.println("1");
+    }
+}
